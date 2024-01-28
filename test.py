096e9738b171c35b05f83d9efefcd1c1c000f502
Time-Coder <binghuiwang5@gmail.com>
2024-01-28 11:56:09 +0800

    update upload script

--- test.py
+++
@@ -1,6 +0,0 @@
-from Connector import Server, Client, Config
-Config.debug = True
-
-server = Server()
-print(server.address)
-server.hold_on()
--- upload.py
+++ upload.py
@@ -1,4 +1,4 @@
 import subprocess
 import sys
 
-subprocess.call(sys.executable, "-m", "twine", "upload", "dist/*")
+subprocess.call([sys.executable, "-m", "twine", "upload", "dist/*"])
